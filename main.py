e8b050cc24baf6ae0f66ab0fc699a3778afd92fb
mk322 <77368272+mk322@users.noreply.github.com>
2022-08-16 22:36:55 -0700

    Update main.py

--- main.py
+++ main.py
@@ -133,27 +133,3 @@ print('Training Time: ', training_time)
 
 
 
-"""
-''' Let us now predict the sentiment on a single sentence just for the testing purpose. '''
-test_sen1 = "This is one of the best creation of Nolan. I can say, it's his magnum opus. Loved the soundtrack and especially those creative dialogues."
-test_sen2 = "Ohh, such a ridiculous movie. Not gonna recommend it to anyone. Complete waste of time and money."
-
-test_sen1 = TEXT.preprocess(test_sen1)
-test_sen1 = [[TEXT.vocab.stoi[x] for x in test_sen1]]
-
-test_sen2 = TEXT.preprocess(test_sen2)
-test_sen2 = [[TEXT.vocab.stoi[x] for x in test_sen2]]
-
-test_sen = np.asarray(test_sen1)
-test_sen = torch.LongTensor(test_sen)
-with torch.no_grad():   
-    test_tensor = Variable(test_sen)
-test_tensor = test_tensor.cuba()
-model.eval()
-output = model(test_tensor, 1)
-out = F.softmax(output, 1)
-if (torch.argmax(out[0]) == 1):
-    print ("Sentiment: Positive")
-else:
-    print ("Sentiment: Negative")
-"""
